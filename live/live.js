b85b77ae29da9bbb18e0d0abde80692c4c408d28
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2019-12-23 23:10:07 +0000

    Changed the way serach works

--- live/live.js
+++ live/live.js
@@ -1,29 +1,45 @@
 const submitButton = document.querySelector('#submitButton');
 const stationList = document.querySelector('#stationList');
 const deptable = document.querySelector('#deptable');
+const stationSearch = document.querySelector('#stationSearch');
+const stations = document.querySelector('#stations');
 
-fetch('http://railmate.net:3060/app/stations')
+const API_URL = 'http://localhost';
+
+let staionData = [];
+
+stationSearch.addEventListener("input", () => {
+    console.log(stationSearch.value);
+    stations.innerHTML = "";
+    for (let i = 0; i < staionData.length; i++) {
+        if (staionData[i].Station_Name.toLowerCase().includes(stationSearch.value)) {
+            addToStations(staionData[i].Station_Name, staionData[i].CRS_Code);
+        }
+    }  
+})
+
+fetch(`${API_URL}:3060/app/stations`)
     .then(response => response.json())
     .then((data) => {
         console.log(data);
-
+        staionData = data.test;
         for (let i = 0; i < data.test.length; i++) {
-            addToList(data.test[i].Station_Name, data.test[i].CRS_Code);
+            //addToList(data.test[i].Station_Name, data.test[i].CRS_Code);
+            addToStations(data.test[i].Station_Name, data.test[i].CRS_Code);
         }
-
 
     })
     .catch((err) => {
         console.log(err); 
     });
 
-submitButton.addEventListener("click", () => {
-    var CRS_Code = stationList.options[stationList.selectedIndex].value;
-    console.log(CRS_Code);
+// submitButton.addEventListener("click", () => {
+//     var CRS_Code = stationList.options[stationList.selectedIndex].value;
+//     console.log(CRS_Code);
 
-    getLiveDep(CRS_Code);
+//     getLiveDep(CRS_Code);
 
-});
+// });
 
 function addToList (Station_Name, CRS_Code) {
     let option = document.createElement("option");
@@ -32,8 +48,29 @@ function addToList (Station_Name, CRS_Code) {
     stationList.appendChild(option);
 }
 
+function addToStations (Station_Name, CRS_Code) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.innerText = Station_Name;
+    a.setAttribute("href", CRS_Code)
+    a.setAttribute("value", CRS_Code)
+    a.addEventListener("click", (e) => {
+        event.preventDefault();
+        let target = e.target;
+        console.log(e.srcElement.attributes[1].value);
+        getLiveDep(e.srcElement.attributes[1].value)
+        
+    });
+    // li.setAttribute("value", CRS_Code);
+
+    li.appendChild(a);
+
+    stations.appendChild(li);
+
+}
+
 function getLiveDep (CRS_Code) {
-    fetch(`http://railmate.net:3060/app/livedepatures/${CRS_Code}`)
+    fetch(`${API_URL}:3060/app/livedepatures/${CRS_Code}`)
     .then(response => response.json())
     .then((data) => {
         console.log(data);
@@ -44,7 +81,6 @@ function getLiveDep (CRS_Code) {
             addToTable(data.departures.all[i].destination_name, data.departures.all[i].platform, data.departures.all[i].operator_name, data.departures.all[i].aimed_departure_time);    
         };
 
-
     })
     .catch((err) => {
         console.log(err); 
@@ -52,8 +88,6 @@ function getLiveDep (CRS_Code) {
 }
 
 function addToTable (Des,Platform, Operator, DepTime){
-
-    
 
     let tr = document.createElement('tr');
 
