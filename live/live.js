9a03a4100556c36134705f917da3848303a0b9f2
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2019-12-26 21:54:04 +0000

    Added error message for no depatures found

--- live/live.js
+++ live/live.js
@@ -1,17 +1,20 @@
 const submitButton = document.querySelector('#submitButton');
 const stationList = document.querySelector('#stationList');
 const deptable = document.querySelector('#deptable');
+const deptableheadings = document.querySelector('#deptableheadings');
 const stationSearch = document.querySelector('#stationSearch');
 const stations = document.querySelector('#stations');
+const errorMessage = document.querySelector('#errorMessage');
 
 const API_URL = 'http://railmate.net';
 
 let staionData = [];
 
 stationSearch.addEventListener("input", () => {
+    errorMessage.style.display = "none";
     console.log(stationSearch.value);
     stations.innerHTML = "";
-    console.log(staionData);
+    // console.log(staionData);
     for (let i = 0; i < staionData.length; i++) {
         if (staionData[i].Station_Name.toLowerCase().includes(stationSearch.value.toLowerCase())) {
             addToStations(staionData[i].Station_Name, staionData[i].CRS_Code);
@@ -46,6 +49,7 @@ function addToStations (Station_Name, CRS_Code) {
         console.log(event.srcElement.attributes[1].value);
         getLiveDep(event.srcElement.attributes[1].value);
         stations.innerHTML = "";
+        stationSearch.value = Station_Name;
         
     });
     // li.setAttribute("value", CRS_Code);
@@ -64,19 +68,30 @@ function getLiveDep (CRS_Code) {
 
         deptable.innerHTML = "";
 
-        for (let i = 0; i < data.departures.all.length; i++) {
-            addToTable(data.departures.all[i].destination_name, data.departures.all[i].platform, data.departures.all[i].operator_name, data.departures.all[i].aimed_departure_time);    
-        };
-
+        if (data.departures.all.length == 0) {
+            console.log("There are no depatures in the next 2 hours from this station");
+            errorMessage.style.display = "block";
+            errorMessage.innerText = `Sorry 😥 there are no depatures in the next 2 hours from ${data.station_name}`
+        } else {
+            deptableheadings.style.display = "contents";
+            errorMessage.style.display = "none";
+            for (let i = 0; i < data.departures.all.length; i++) {
+                addToTable(data.departures.all[i].destination_name, data.departures.all[i].platform, 
+                    data.departures.all[i].operator_name, data.departures.all[i].aimed_departure_time, data.departures.all[i].service_timetable.id);    
+            };
+        }
     })
     .catch((err) => {
         console.log(err); 
     });  
 }
 
-function addToTable (Des,Platform, Operator, DepTime){
+function addToTable (Des,Platform, Operator, DepTime, url){
 
     let tr = document.createElement('tr');
+    tr.addEventListener("click", () => {
+        console.log("clicked");
+    })
 
     let tdDes = document.createElement('td');
     tdDes.innerHTML  = Des;
