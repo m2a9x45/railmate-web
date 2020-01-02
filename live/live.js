1760489eddf87c48274b4b933fbcb997c2ed3616
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2020-01-02 00:56:00 +0000

    debugging

--- live/live.js
+++ live/live.js
@@ -106,7 +106,9 @@ function addToTable (Des,Platform, Operator, DepTime, url){
         getTrainRoute(e.path[1].attributes[0].value);
     })
     tr.addEventListener("touchstart", (e) => {
-        console.log(e.composedPath()[1]);
+        console.log(e.composedPath());
+        
+        console.log(e.composedPath()[1].value);
         
     })
 
